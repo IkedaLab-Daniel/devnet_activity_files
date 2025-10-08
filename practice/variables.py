5cf946a6076ac8fbf414ac0953fbb53e9bebdfa5
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-08 23:28:18 +0800

    practice: variables

---
+++ practice/variables.py
@@ -0,0 +1,33 @@
+print("Variable practice")
+# > int
+myInt = 1
+
+# > string
+myString = "string"
+
+# > float
+myFloat = 3.14
+
+# > boolean
+myBool = True
+
+# > list
+myList = [1, 2, 3, 4, 5]
+
+# > dictionary
+myDict = {
+    "key": "value",
+    "name": "John"
+}
+
+# > tuple
+myTuple = (1, 2, 3)
+
+# ? Print all variables
+print("Integer:", myInt)
+print("String:", myString)
+print("Float:", myFloat)
+print("Boolean:", myBool)
+print("List:", myList)
+print("Dictionary:", myDict)
+print("Tuple:", myTuple)
